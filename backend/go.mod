3834e8aa1d3b276d56cd571e379f01ed6cfe0b89
Mrityunjay Roy <royr91599@gmail.com>
2026-08-03 18:39:26 +0530

    added database config

--- backend/go.mod
+++ backend/go.mod
@@ -4,24 +4,22 @@ go 1.25.5
 
 require (
 	github.com/go-playground/validator/v10 v10.30.1
-	github.com/jackc/pgx/v4 v4.18.3
+	github.com/jackc/pgx-zerolog v0.0.0-20230315001418-f978528409eb
+	github.com/jackc/pgx/v5 v5.10.0
 	github.com/joho/godotenv v1.5.1
 	github.com/knadh/koanf/providers/env v1.1.0
 	github.com/knadh/koanf/v2 v2.3.0
+	github.com/newrelic/go-agent/v3/integrations/nrpgx5 v1.3.4
 	github.com/rs/zerolog v1.34.0
 )
 
 require (
-	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
-	github.com/jackc/pgconn v1.14.3 // indirect
-	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgtype v1.14.0 // indirect
-	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/newrelic/go-agent/v3/integrations/logcontext-v2/nrwriter v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sync v0.19.0 // indirect
 )
 
 require (
@@ -35,13 +33,13 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
-	github.com/newrelic/go-agent/v3 v3.42.0
+	github.com/newrelic/go-agent/v3 v3.43.3
 	github.com/newrelic/go-agent/v3/integrations/logcontext-v2/zerologWriter v1.0.5
-	golang.org/x/crypto v0.46.0 // indirect
-	golang.org/x/net v0.47.0 // indirect
-	golang.org/x/sys v0.39.0 // indirect
-	golang.org/x/text v0.32.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
-	google.golang.org/grpc v1.65.0 // indirect
-	google.golang.org/protobuf v1.34.2 // indirect
+	golang.org/x/crypto v0.47.0 // indirect
+	golang.org/x/net v0.49.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/text v0.33.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260120221211-b8f7ae30c516 // indirect
+	google.golang.org/grpc v1.80.0 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 )
--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -1,8 +1,20 @@
 package database
 
 import (
-	"github.com/jackc/pgx/v4/pgxpool"
-	"github.com/jackc/pgx/v4/pgxpool"
+	"context"
+	"fmt"
+	"net"
+	"net/url"
+	"strconv"
+	"time"
+
+	"github.com/MrityunjayRoy/prod-go-boilerplate/internal/config"
+	loggerConfig "github.com/MrityunjayRoy/prod-go-boilerplate/internal/logger"
+	pgxzero "github.com/jackc/pgx-zerolog"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/tracelog"
+	"github.com/newrelic/go-agent/v3/integrations/nrpgx5"
 	"github.com/rs/zerolog"
 )
 
@@ -11,4 +23,100 @@ type Database struct {
 	log *zerolog.Logger
 }
 
+type multiTracer struct {
+	tracers []any
+}
+
 const DatabasePingTimeout = 10
+	
+func (mt *multiTracer) TraceQueryStart(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryStartData) context.Context {
+	for _, tracer := range mt.tracers {
+		if t, ok := tracer.(interface {
+			TraceQueryStart(context.Context, *pgx.Conn, pgx.TraceQueryStartData) context.Context
+		}); ok {
+			ctx = t.TraceQueryStart(ctx, conn, data)
+		}
+	}
+	return ctx
+}
+
+func (mt *multiTracer) TraceQueryEnd(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryEndData) context.Context {
+	for _, tracer := range mt.tracers {
+		if t, ok := tracer.(interface {
+			TraceQueryStart(context.Context, *pgx.Conn, pgx.TraceQueryEndData) context.Context
+		}); ok {
+			ctx = t.TraceQueryStart(ctx, conn, data)
+		}
+	}
+	return ctx
+}
+
+func New(cfg *config.Config, logger *zerolog.Logger, loggerService *loggerConfig.LoggerService) (*Database, error) {
+	hostPort := net.JoinHostPort(cfg.Database.Host, strconv.Itoa(cfg.Database.Port))
+
+	// url-encode the password
+	encodedPassword := url.QueryEscape(cfg.Database.Password)
+	dsn := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=%s",
+		cfg.Database.User,
+		encodedPassword,
+		hostPort,
+		cfg.Database.Name,
+		cfg.Database.SSLMode,
+	)
+
+	pgxPoolConfig, err := pgxpool.ParseConfig(dsn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse pool config: %w", err)
+	}
+
+	if loggerService != nil && loggerService.GetApplication() != nil {
+		pgxPoolConfig.ConnConfig.Tracer = nrpgx5.NewTracer()
+	}
+
+	if cfg.Primary.Env == "local"{
+		globalLevel := logger.GetLevel()
+		pgxLogger := loggerConfig.NewPgxLogger(globalLevel)
+
+		if pgxPoolConfig.ConnConfig.Tracer != nil {
+			// If new relic tracer exists, create a multi-tracer
+			localTracer := &tracelog.TraceLog{
+				Logger: pgxzero.NewLogger(pgxLogger),
+				LogLevel: tracelog.LogLevel(loggerConfig.GetPgxTraceLevel(globalLevel)),
+			}
+			pgxPoolConfig.ConnConfig.Tracer = &multiTracer{
+				tracers: []any{pgxPoolConfig.ConnConfig.Tracer, localTracer},
+			}
+		}else {
+			pgxPoolConfig.ConnConfig.Tracer = &tracelog.TraceLog{
+				Logger: pgxzero.NewLogger(pgxLogger),
+				LogLevel: tracelog.LogLevel(loggerConfig.GetPgxTraceLevel(globalLevel)),
+			}
+		}
+	}
+
+	pool, err := pgxpool.NewWithConfig(context.Background(), pgxPoolConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create pgx pool: %w", err)
+	}
+
+	database := &Database{
+		Pool: pool,
+		log: logger,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), DatabasePingTimeout*time.Second)
+	defer cancel()
+	if err = pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("failed to ping database: %w", err)
+	}
+
+	logger.Info().Msg("Connected to the database")
+
+	return database, nil
+}
+
+func (db *Database) Close() error {
+	db.log.Info().Msg("Closing database connection pool...")
+	db.Pool.Close()
+	return nil
+}
